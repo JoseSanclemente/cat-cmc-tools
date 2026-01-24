07d36d0b541249dbcefbdb991fb38bce0488eaf1
JoseSanclemente <jsanclemente20@gmail.com>
2026-01-24 15:18:38 -0500

    Create a back button to go back to the index

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ class Header extends HTMLElement {
   }
 
   static get observedAttributes() {
-    return ["title"];
+    return ["title", "backButton"];
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
@@ -15,6 +15,9 @@ class Header extends HTMLElement {
   connectedCallback() {
     const shadowDOM = this.attachShadow({ mode: "open" });
 
+    const component = document.querySelector("header-component");
+    const isBackButton = component.hasAttribute("backButton");
+
     shadowDOM.innerHTML = `
     <style>
       header {
@@ -22,13 +25,14 @@ class Header extends HTMLElement {
         align-items: center;
         justify-content: center;
       }
+        
       .header-logo {
         background-image: url("https://brandlogos.net/wp-content/uploads/2022/09/cat-logo_brandlogos.net_tl5q3-512x512.png");
         background-size: contain;
         background-repeat: no-repeat;
         height: 60px;
         width: 60px;
-        margin-right: 16px;
+        margin: 0 24px;
       }
 
       .header-title {
@@ -36,8 +40,21 @@ class Header extends HTMLElement {
         font-size: 2rem;
         font-weight: bold;
       }
+
+      .header-back {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
     </style>
     <header>
+      ${
+        isBackButton
+          ? `<a class="header-back" href="../index.html">
+        <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#000000"><path d="M560.67-240 320-480.67l240.67-240.66L608-674 414.67-480.67 608-287.33 560.67-240Z"/></svg>
+      </a>`
+          : ""
+      }
       <div class="header-logo"></div>
       <h1 class="header-title">${this.title}</h1>
     </header>
